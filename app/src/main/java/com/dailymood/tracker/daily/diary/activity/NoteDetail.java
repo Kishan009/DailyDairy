193ba2b2908afaaad3fef613fadb9cadfd8bdeab
viraj470 <40735319+viraj470@users.noreply.github.com>
2018-11-14 18:06:26 +0530

    Note Detail Half done

---
+++ app/src/main/java/com/dailymood/tracker/daily/diary/activity/NoteDetail.java
@@ -0,0 +1,16 @@
+
+package com.dailymood.tracker.daily.diary.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dailymood.tracker.daily.diary.R;
+
+public class NoteDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_detail);
+    }
+}
